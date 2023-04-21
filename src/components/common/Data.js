a19f5f11f6743a0e68f1a781f9014d1049dfaea0
maf256 <maf256@gmail.com>
2023-04-22 01:13:33 +0200

    add Location in SVG

--- src/components/common/Data.js
+++ src/components/common/Data.js
@@ -56,7 +56,6 @@ export const Data = {
             {name: 'Skills', href: '#skills'},
             {name: 'Contact', href: '#contact'},
             {name: 'Resume', href: 'https://docs.google.com/document/d/1CnW2JXswDhA3hJBSsXhzZ98TOPY6wbgO/edit?usp=sharing&ouid=109271537866387535039&rtpof=true&sd=true'},
-            // https://docs.google.com/document/d/1CnW2JXswDhA3hJBSsXhzZ98TOPY6wbgO/edit?usp=sharing&ouid=109271537866387535039&rtpof=true&sd=true
             ],
         nr: [
             {name: 'Hjem', href: '#home'},
